73a40caf14e464c6b714eea56cbe6b40ed0bc2ac
sevaradeveloper <sevara123@icloud.com>
2025-09-13 13:12:54 +0300

    qolgani

---
+++ js/main.js
@@ -0,0 +1,295 @@
+// Boolean1. A butun soni berilgan. Jumlani rostlikka tekshiring: "A soni musbat". 
+
+// let a = Number(prompt("a sonini kiriting"));
+
+// if ( a > 0) {
+//     alert("mustbat")
+// } else {
+//    alert("manfiy")
+// }
+
+
+
+
+// Boolean2. A butun soni berilgan. Jumlani rostlikka tekshiring: "A soni toq son". 
+
+// let a = Number(prompt("a sonini kiriting"));
+
+// if ( a % 2) {
+//     alert("toq")
+// } else {
+//    alert("juft")
+// }
+
+
+
+// Boolean3. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A>2 va B <=3"
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if ( a > 2 && b<=3) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+// Boolean4. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A <= B <= C"
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a<=b && b<=c) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+//Boolean5. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlarining har ikkalasi ham yoki toq son yoki juft son".
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if ( a % 2 === b % 2 ) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+// Boolean6. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlarning hech bo'lmaganda bittasi musbat".
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a>0 || b>0 || c>0) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+
+// Boolean7. Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonning barcha raqamlari har xil".
+
+
+// chunmadim
+
+
+// Boolean8. Jumlani rostlikka tekshiring: "Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir biriga teng".
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a===b ||a===c || b===c) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+
+
+// UYGA VAZIFA
+// Boolean1. A butun soni berilgan. Jumlani rostlikka tekshiring: "A soni juft son”.
+
+// let a = Number(prompt("a sonini kiriting"));
+
+// if (a % 2 === 0 ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+
+
+// Boolean2. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring:
+// "A >= 0 yoki B <-2"
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if ( a > 0 && b <-2 ) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+// Boolean3. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "B soni A va C sonlari orasida yotadi".
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( (a<b && b<c) || (a>b && b>c)) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+
+// Boolean4. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlari toq sonlar".
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if (a % 2 && b%2 ) {
+//     alert("toq")
+// } else {
+//     alert("juft")
+// }
+
+
+// Boolean5. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlarning hech bo'lmaganda bittasi toq son".
+
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if ((a % 2 && b>0) ||(a >0 && b%2) ) {
+//     alert("toq")
+// } else {
+//     alert("juft")
+// }
+
+
+// Boolean6. Ikkita butun A va B sonlari berilgan. Jumlani rostlikka tekshiring: "A va B sonlarning faqat bittasi toq son".
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+
+// if ((a % 2 ) ||(b%2) ) {
+//     alert("toq")
+// } else {
+//     alert("juft")
+// }
+
+
+// Boolean7. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlarning har biri musbat".
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a>0 && b>0 && c>0) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+// Boolean8. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlaridan faqat bittasi musbat son".
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a<0  || b>0||  c<0) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+// ---------------------------chunmadim
+
+// Boolean9. Uchta A, B, C butun sonlar berilgan. Jumlani rostlikka tekshiring: "A, B, C sonlardan faqat ikkitasi musbat son".
+
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a<0  || b>0||  c<0) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
+
+
+// Boolean10. Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son ikki xonali juft son"
+
+
+// let a = Number(prompt("a sonini kiriting"));
+
+
+// if (a >=10 && a<99 && a % 2 === 0  ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+
+// Boolean11. Musbat butun son berilgan. Jumlani rostlikka tekshiring: "Berilgan son uch xonali toq". 
+
+
+// let a = Number(prompt("a sonini kiriting"));
+
+
+// if (a >=100 && a<999 && a % 2   ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+
+
+// Boolean12. Jumlani rostlikka tekshiring: "Berilgan uchta butun sonlarning hech bo'lmaganda bir jufti o'zaro qarama-qarshi".
+
+
+
+
+// Boolean13. Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonning raqamlari ketama- ket o'suvchi bo'lib joylashgan".
+
+// let N = Number(prompt("N sonini kiriting"));359
+// let a = Math.floor(N/100); 3
+// let b = Math.floor((N/10)%10);5
+// let c = Math.floor(N%10);9
+
+
+// if ( a<b && b<c  ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+// Boolean14. Uch xonali son berilgan. Jumlani rostlikka tekshiring: "Ushbu sonning raqamlari ketama- ket o'suvchi bo'lib joylashgan yoki kamayuvchi ketma-ketlikka ega".
+
+
+// let N = Number(prompt("N sonini kiriting"));359
+// let a = Math.floor(N/100); 3
+// let b = Math.floor((N/10)%10);5
+// let c = Math.floor(N%10);9
+
+
+// if ( (a<b && b<c)|| (a>b && b>c)  ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
+// Boolean15. Uch xonali son berilgan. Jumlani rostlikka tekshiring: "Ushbu sonni chapdan o'qiganda ham, o'ngdan o'qiganda ham bir xil".
+
+// let N = Number(prompt("N sonini kiriting"));359
+// let a = Math.floor(N/100); 3
+// let b = Math.floor((N/10)%10);5
+// let c = Math.floor(N%10);9
+
+
+// if ( c == a ) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
