aad82e906c01f50e3210fd524e977334c5e498a7
sevaradeveloper <sevara123@icloud.com>
2025-09-16 13:16:09 +0300

    qolgani

--- js/main.js
+++ js/main.js
@@ -79,8 +79,18 @@
 
 // Boolean7. Uch xonali son berilgan. Jumlani rostlikka tekshiring: “Ushbu sonning barcha raqamlari har xil".
 
+// let N = Number(prompt("N sonini kiriting"));359
+// let a = Math.floor(N/100); 3
+// let b = Math.floor((N/10)%10);5
+// let c = Math.floor(N%10);9
 
-// chunmadim
+
+// if ( a !== b  && a !== c && c !== b) {
+//     alert("true")
+// } else {
+//     alert("false")
+// }
+
 
 
 // Boolean8. Jumlani rostlikka tekshiring: "Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir biriga teng".
@@ -211,10 +221,15 @@
 // let b = Number(prompt("b sonini kiriting"));
 // let c = Number(prompt("c sonini kiriting"));
 
-// if ( a<0  || b>0||  c<0) {
+
+// if (a >= 0 && b >= 0 && c < 0 ) {
 //     alert("true")
-// } else {
-//    alert("false")
+// } else if(a >= 0 && b < 0 && c >= 0) {
+//     alert("true")
+// }else if(a < 0 && b >= 0 && c >= 0 ){
+//      alert("true")
+// }else{
+//     alert("false")
 // }
 
 
@@ -237,7 +252,7 @@
 // let a = Number(prompt("a sonini kiriting"));
 
 
-// if (a >=100 && a<999 && a % 2   ) {
+// if (a % 2  !==0 ) {
 //     alert("true")
 // } else {
 //     alert("false")
@@ -247,6 +262,15 @@
 
 // Boolean12. Jumlani rostlikka tekshiring: "Berilgan uchta butun sonlarning hech bo'lmaganda bir jufti o'zaro qarama-qarshi".
 
+// let a = Number(prompt("a sonini kiriting"));
+// let b = Number(prompt("b sonini kiriting"));
+// let c = Number(prompt("c sonini kiriting"));
+
+// if ( a === -b || a=== -c || b=== -c ) {
+//     alert("true")
+// } else {
+//    alert("false")
+// }
 
 
 
@@ -293,3 +317,323 @@
 //     alert("false")
 // }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if1. Butun son berilgan. Agar berilgan son musbat bo`lsa, 1 ga oshiring, aks holda 2 ga kamaytiring. Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// let n = Number(prompt("Butun son kiriting:"));
+// if (n > 0) {
+//   n = n + 1;
+// } else {
+//   n = n - 2;
+// }
+// alert("Natija: " + n);
+
+
+
+// if2. Uchta butun son berilgan. Shu sonlar orasidan nechta musbat va manfiy son borligini aniqlovchi programma tuzilsin.
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+// let c = Number(prompt("3-sonni kiriting:"));
+
+// let musbat = 0, manfiy = 0;
+
+// if (a > 0) musbat++; else if (a < 0) manfiy++;
+// if (b > 0) musbat++; else if (b < 0) manfiy++;
+// if (c > 0) musbat++; else if (c < 0) manfiy++;
+
+// alert("Musbat sonlar: " + musbat + "\nManfiy sonlar: " + manfiy);
+
+
+
+
+
+// if3. Uchta son berilgan. Shu sonlarni avval kichigini keyin kattasini ekranga chiqaruvchi programma tuzilsin.
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+// let c = Number(prompt("3-sonni kiriting:"));
+
+// let min = Math.min(a, b, c);
+// let max = Math.max(a, b, c);
+
+// alert("Kichik son: " + min + "\nKatta son: " + max);
+
+
+
+
+// if4. Uchta butun son berilgan. Shu sonlarni ikkitasi o`zaro teng, qolgan bittasini tartib raqami aniqlansin.
+
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+// let c = Number(prompt("3-sonni kiriting:"));
+
+// if (a === b && a !== c) {
+//   alert("Qolgan son: 3-chi");
+// } else if (a === c && a !== b) {
+//   alert("Qolgan son: 2-chi");
+// } else if (b === c && b !== a) {
+//   alert("Qolgan son: 1-chi");
+// } else {
+//   alert("Ikkitasi ham teng emas!");
+// }
+
+
+
+
+
+
+
+// if5. x haqiqiy soni berilgan. Quyidagi funksiya hisoblansin.
+
+// agar x < -2 yoki x > 2 bo’lsa 2 * x 
+// aks holda -3 * x;
+
+
+
+// let x = Number(prompt("x sonini kiriting:"));
+// let y;
+
+// if (x < -2 || x > 2) {
+//   y = 2 * x;
+// } else {
+//   y = -3 * x;
+// }
+
+// alert("Natija: " + y);
+
+// if6. Butun son berilgan. Berilgan sonni "musbat toq", "musbat juft", "nol", “manfiy juft”, “manfiy toq” ekranga yozadigan programma tuzilsin.
+
+
+// let n = Number(prompt("Butun son kiriting:"));
+
+// if (n === 0) {
+//   alert("nol");
+// } else if (n > 0 && n % 2 === 0) {
+//   alert("musbat juft");
+// } else if (n > 0 && n % 2 !== 0) {
+//   alert("musbat toq");
+// } else if (n < 0 && n % 2 === 0) {
+//   alert("manfiy juft");
+// } else {
+//   alert("manfiy toq");
+// }
+
+
+
+
+// UYGA VAZIFA
+// if1. Butun son berilgan. Agar, berilgan son musbat+ bo`lsa, 1 ga oshirilsin, aks (-minus) holda o`zgartirilmasin. Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin.
+
+
+// let n = Number(prompt("Butun son kiriting:"));
+// if (n > 0) {
+//   n = n + 1;
+// } else {
+//   n = n ;
+// }
+// alert("Natija: " + n);
+
+
+// if2. Butun son berilgan. Agar, berilgan son musbat bo`lsa, 1 ga oshiring, agar manfiy bo`lsa 2 ga kamaytiring. Agar 0 ga teng bo`lsa, 10 ni o`zlashtirsin. Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin
+
+
+
+// let n = Number(prompt("Butun son kiriting:"));
+
+// if (n > 0) {
+//   n = n + 1;
+// } else if (n<0) {
+//   n = n-2;
+// }else{
+//   n = 10;
+// }
+// alert("Natija: " + n);
+
+
+
+
+// if3. Uchta butun son berilgan. Shu sonlar orasidan nechta musbat son borligini aniqlovchi programma tuzilsin.
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+// let c = Number(prompt("3-sonni kiriting:"));
+
+// let musbat = 0;
+
+// if (a > 0) musbat++; else;
+// if (b > 0) musbat++; else ;
+// if (c > 0) musbat++; else ;
+
+// alert("Musbat sonlar: " + musbat);
+
+
+// if4. Ikkita butun son berilgan. Shu sonlarning kattasini aniqlovchi programma tuzilsin.
+
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+
+// let max = Math.max(a, b);
+
+// alert("Katta son: " + max);
+
+
+
+
+
+
+
+// if5. Ikkita butun son berilgan. Shu sonlarning kichigini tartib raqamini aniqlovchi programma tuzilsin.
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+
+// if (a>b) {
+//     alert("tartib boyicha 2-raqam eng kichigi"+b)
+// } else {
+//     alert("tartib boyicha 1-raqam eng kichigi"+a)
+// }
+
+
+// if6. Ikkita butun son berilgan. Shu sonlarning avval kattasini keyin kichigini ekranga chiqaruvchi programma tuzilsin.
+
+
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+
+
+// let min = Math.min(a, b);
+// let max = Math.max(a, b);
+
+// alert("Katta son: " + max + "Kichik son: " + min);
+
+
+
+
+
+// if7. A va B haqiqiy sonlari beringan. Shu sonlarni shunday o'zgartirish kerakki, A son kichik B son katta bo`lsin. A va B ning qiymati ekranga chiqarilsin.
+
+// let A = Number(prompt("A sonini kiriting:"));
+// let B = Number(prompt("B sonini kiriting:"));
+
+// let kichik = Math.min(A, B); // har doim kichik sonni oladi
+// let katta = Math.max(A, B); // har doim katta sonni oladi
+
+// A = kichik;
+// B = katta;
+
+// alert("A = " + A + ", B = " + B);
+
+
+// if8. A va B butun sonlari berilgan. Agar o`zgaruvchilar o`zaro teng bo`lmasa, A va B o`zgaruvchilari ularning yig'indisini o`zlashtirsin. Agar teng bo`lsa, 0 ni o`zlashtirsin. A va B ning qiymati ekranga chiqarilsin.
+
+// let A = Number(prompt("A sonini kiriting:"));
+// let B = Number(prompt("B sonini kiriting:"));
+
+// if (A !== B) {
+//     let n=A+B;
+//     A=n;
+//     B=n;
+//     alert("n="+n+"A="+A+"B="+B)
+// } else {
+//     let n=0;
+//     A=n;
+//     B=n;
+//     alert("teng="+n+"A="+A+"B="+B);
+// }
+
+
+
+// if9. A va B butun sonlari berilgan. Agar o`zgaruvchilar o`zaro teng bo`lmasa, A va B bu sonlarning kattasini o`zlashtirsin. Agar teng bo`lsa, 0 ni o`zlashtirsin. A va B ning qiymati ekranga chiqarilsin.
+
+
+
+// let A = Number(prompt("A sonini kiriting:"));
+// let B = Number(prompt("B sonini kiriting:"));
+
+// if (A !== B) {
+//     let n=Math.max(A,B);
+//     A=n;
+//     B=n;
+//     alert("n="+n+"A="+A+"B="+B)
+// } else {
+//     let n=0;
+//     A=n;
+//     B=n;
+//     alert("teng="+n+"A="+A+"B="+B);
+// }
+
+
+
+// if10. Uchta son berilgan. Shu sonlarni kichigini aniqlovchi programma tuzilsin.
+
+
+// let a = Number(prompt("1-sonni kiriting:"));
+// let b = Number(prompt("2-sonni kiriting:"));
+// let c = Number(prompt("3-sonni kiriting:"));
+
+// let min = Math.min(a, b, c);
+
+// alert("Kichik son: " + min );
